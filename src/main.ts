88fa7fcadcf1d01d41d526df2b71ce0da4881e25
hoangit2k2 <nguyenhoang2002it@gmail.com>
2023-06-02 22:09:16 +0700

    deploy again

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: 'http://localhost:4200' });
   const config = new DocumentBuilder()
     .setTitle('Quy Thanh Pubs')
     .setDescription('API Documentation')
