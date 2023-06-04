6d4bb518a8142acd3bd408782d3c904c3df16364
hoangit2k2 <nguyenhoang2002it@gmail.com>
2023-06-04 10:26:38 +0700

    upload

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: 'http://localhost:4200' });
-  app.enableCors({ origin: 'https://quythanh-zlii.onrender.com' });
+  // app.enableCors({ origin: 'https://quythanh-zlii.onrender.com' });
+  const corsOptions: CorsOptions = {
+    origin: 'https://quythanh-zlii.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  app.use(cors(corsOptions));
   const config = new DocumentBuilder()
     .setTitle('Quy Thanh Pubs')
     .setDescription('API Documentation')
