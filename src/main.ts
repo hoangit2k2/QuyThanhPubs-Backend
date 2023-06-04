189b975c112ae3cbe5df06f3b115633267b1cd59
hoangit2k2 <nguyenhoang2002it@gmail.com>
2023-06-04 10:36:10 +0700

    fix crod

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,14 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import * as cors from 'cors';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({ origin: 'http://localhost:4200' });
-  app.enableCors({ origin: 'https://quythanh-zlii.onrender.com' });
-  // const corsOptions: CorsOptions = {
-  //   origin: 'https://quythanh-zlii.onrender.com',
-  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  //   allowedHeaders: ['Content-Type', 'Authorization'],
-  // };
-  // app.use(cors(corsOptions));
+  const corsOptions: CorsOptions = {
+    origin: 'https://quythanh-zlii.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  app.use(cors(corsOptions));
   const config = new DocumentBuilder()
     .setTitle('Quy Thanh Pubs')
     .setDescription('API Documentation')
