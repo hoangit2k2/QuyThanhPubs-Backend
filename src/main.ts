6f126da40fb2b76bb6f739a70954b15eabe39a3e
hoangit2k2 <nguyenhoang2002it@gmail.com>
2023-06-04 10:15:40 +0700

    upload

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({ origin: 'http://localhost:4200' });
+  app.enableCors({ origin: 'http://localhost:4200' });
   app.enableCors({ origin: 'https://quythanh-zlii.onrender.com' });
   const config = new DocumentBuilder()
     .setTitle('Quy Thanh Pubs')
